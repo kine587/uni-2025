d4c8c78f8eeb7ea8d583b9f9018e62ec5fe2c1de
kine <kine.kodehode@gmail.com>
2025-06-23 13:51:40 +0200

    intro til javascript

---
+++ script.js
@@ -0,0 +1,138 @@
+// variables and opperators
+
+// section 1 commenting our code
+
+// shift alt a hot key for comment
+/* jdkg
+sldgfos
+sdhfsl */
+
+// console log
+console.log("Hello World");
+
+// variables and data types here we will see different types
+
+// shift alt down arrow duplicate line
+
+exampleText = "this is my variable text";
+console.log(exampleText);
+console.log(exampleText);
+console.log(exampleText);
+
+// string (text)
+// camel text first word lowercase and second word uppercase
+
+let exampleText = "5";
+console.log(exampleText);
+
+// integer (number)
+
+const myNum = 5;
+console.log(myNum);
+
+// booleon (true or false)
+
+booleoanOne = true;
+BooleanTwo = false;
+console.log(booleoanOne);
+console.log(BooleanTwo);
+
+//array (list of items)
+// index is the possision of the item in the array, starting at 0
+
+exampleArray = [true, 100, "hello"];
+
+console.log(exampleArray[1]);
+
+// object
+
+/* person = ( 
+    name: "joe",
+    age: 37
+);
+ */
+console.log[person];
+
+// undefying
+let undefindExample;
+console.log(undefinedExample);
+
+// null
+nullExample(nullExample);
+console.log(nullExample);
+
+// let and const const is constant let can be variable
+// declaring variables
+
+let Changeablestring = "i can change";
+console.log(Changeablestring);
+
+Changeablestring = "ive changed";
+console.log(Changeablestring);
+
+const fixedString = "i can not be reclared";
+console.log(fixedString);
+
+/*fixedString = "im attemting to change";
+console.log(fixedString);*/
+
+let healthpoints = 100;
+console.log(healthpoints);
+
+healthpoints = 50;
+console.log(healthpoints);
+
+const myArray = [100, 5, 25];
+console.log(myArray);
+myArray[1] = 400;
+console.log(myArray);
+
+// basic opperators
+
+let num1 = 10;
+let num2 = 5;
+console.log(num1, num2);
+
+console.log(num1 + num2); //addiction 15
+console.log(num1 - num2); //subtraction 5
+console.log(num1 * num2); //multiplication 50
+console.log(num1 / num2); //division 2
+console.log(num1 % num2); // what remains
+
+//using the + operator with the string
+
+let firstname = "john";
+let lastname = "doe";
+let fullname = firstname + " " + lastname;
+console.log(fullname);
+
+let counter = 0;
+console.log(counter);
+counter++;
+counter++;
+counter++;
+counter++;
+console.log(counter);
+counter--;
+counter--;
+console.log(counter);
+
+counter = counter + 5;
+console.log(counter);
+// this is shorthand
+counter += 5;
+console.log(counter);
+
+let score = 10;
+//subtraction
+score -= 3;
+console.log(score);
+//multiple
+score *= 2;
+console.log(score);
+//division
+score /= 4;
+console.log(score);
+// reminder
+score %= 5;
+console.log(score);
