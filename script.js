c718c8cf82c6ae862ff065bb7a19d7165ecbf0ce
kine <kine.kodehode@gmail.com>
2025-08-07 11:56:23 +0200

    orginazed folder and added a script file for the new lesson

--- script.js
+++
@@ -1,7 +0,0 @@
-function buttonClick() {
-  box = document.createElement("div");
-  box.style.backgroundColor = "blue";
-  box.style.width = "100px";
-  box.style.height = "100px";
-  document.body.append(box);
-}
